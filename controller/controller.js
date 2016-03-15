fa4215bbff46e6269104969018a6e02e73c0c171
Mark-A-A <markawai01@gmail.com>
2016-03-15 18:47:42 -0400

    pushing cheerio to parse thumbnail url

--- controller/controller.js
+++ controller/controller.js
@@ -28,19 +28,28 @@ router.get('/', function (req, res) {
 router.get('/scraper', function (req,res){
   request('http://www.nytimes.com/pages/realestate/index.html', function (error, response,  html) {
     var $ = cheerio.load(html);
-    
+    // var databaseModelurl = 
+    // var collections = [articles]
 
     if (!error && response.statusCode == 200) {
     //console.log(html) // Show the HTML for the page. 
       debugger
-      $('h3').each(function (i, elem) {
+      $('.story').each(function (i, elem) {
+      //$('h3').each(function (i, elem) {
         console.log("scrapping page");
         console.log("this: ", this);
-        var articleTitle = $(this).children('a').text();
-        var articleURL = $(this).children('a').attr('href');
+        var articleTitle = $(this).children('h3').children('a').text();
+        var articleURL = $(this).children('h3').children('a').attr('href');
+        var articleThumbnail = $(this).children('.thumbnail').children('a').children('img').attr('src');
+        var articleThumbnailText = $(this).children('.thumbnail').children('a').children('img').attr('alt');
         console.log("articleTitle: ", articleTitle);
         console.log("articleURL: ", articleURL);
+        console.log("articleThumbnail: ", articleThumbnail);
+        console.log("articleThumbnailText: ", articleThumbnailText);
+
+        //mongooseModel.articles.save()
       });
+      
     } else {
       console.log("error: ", error)
     };
